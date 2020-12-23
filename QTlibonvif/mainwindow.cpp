8f48c8da36b1faa60c8e02015f43c99298d0f53d
fiz1962 <fiz60@hotmail.com>
2020-12-23 09:44:12 -0600

    Do getCapabilities() before getProfile() to fill in some fields

--- QTlibonvif/mainwindow.cpp
+++ QTlibonvif/mainwindow.cpp
@@ -66,23 +66,42 @@ void MainWindow::on_pushButton_2_clicked()
       ui->textEdit->append( onvif_data->xaddrs );
       ui->textEdit->append( onvif_data->device_service );
       ui->Host->addItem(onvif_data->xaddrs);
-
-      /*fprintf(stdout, "enter username:");
-      fgets(onvif_data->username, 128, stdin);
-      fprintf(stdout, "enter password:");
-      fgets(onvif_data->password, 128, stdin);
-
-      onvif_data->username[strcspn(onvif_data->username, "\r\n")] = 0;
-      onvif_data->password[strcspn(onvif_data->password, "\r\n")] = 0;
-
-      if (fillRTSP(onvif_data) == 0)
-          fprintf(stdout, "%s\n", onvif_data->stream_uri);
-      else
-          fprintf(stderr, "Error getting camera uri - %s\n", onvif_data->last_error);
-*/
     }
 
     closeSession(onvif_session);
     free(onvif_session);
     free(onvif_data);
 }
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    struct OnvifSession *onvif_session = (struct OnvifSession*)malloc(sizeof(struct OnvifSession));
+    struct OnvifData *onvif_data = (struct OnvifData*)malloc(sizeof(struct OnvifData));
+    initializeSession(onvif_session);
+
+    QString onvifHost = ui->Host->currentText();
+    QString user = ui->UserName->text();
+    QString pass = ui->Password->text();
+
+    clearData(onvif_data);
+    strncpy(onvif_data->camera_name, "UNKNOWN CAMERA", strlen(onvif_data->camera_name));
+    strcpy(onvif_data->device_service, "POST /onvif/device_service HTTP/1.1");
+    strcpy(onvif_data->xaddrs, onvifHost.toStdString().c_str());
+
+    strcpy(onvif_data->username, user.toStdString().c_str());
+    strcpy(onvif_data->password, pass.toStdString().c_str());
+
+    extractOnvifService(onvif_data->device_service, true);
+    getTimeOffset(onvif_data);
+    onvif_data->time_offset = 0;
+
+    // getCapabilities fills in service addresses
+    getCapabilities(onvif_data);
+
+    saveProfiles("Profiles.txt", onvif_data);
+
+    closeSession(onvif_session);
+    free(onvif_session);
+    free(onvif_data);
+}
+
--- QTlibonvif/mainwindow.h
+++ QTlibonvif/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,6 +21,8 @@ private slots:
     void on_pushButton_clicked();
 
     void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
 
 private:
     Ui::MainWindow *ui;
